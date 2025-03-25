a03d344cbb597cb436e39386d1665451e802670d
AdityaC4 <adityapchaudhari@gmail.com>
2025-03-25 18:17:06 -0400

    frontend

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,12 +1,14 @@
+from typing import Tuple
+
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from third_parties import scrape_linkedin_profile
 from agents import lookup as linkedin_lookup_agent
-from output_parser import summary_parser
+from output_parser import summary_parser, Summary
 
 
-def ice_breaker_with(name: str) -> str:
+def ice_breaker_with(name: str) -> Tuple[Summary, str]:
     linkedin_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_url, mock=True
@@ -20,7 +22,7 @@ def ice_breaker_with(name: str) -> str:
     """
 
     summary_prompt_template = PromptTemplate(
-        input_varaiables=["information"],
+        input_variables=["information"],
         template=summary_template,
         partial_variables={
             "format_instructions": summary_parser.get_format_instructions()
@@ -32,9 +34,9 @@ def ice_breaker_with(name: str) -> str:
     # chain = summary_prompt_template | llm
     chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res: Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("photoUrl")
 
 
 if __name__ == "__main__":
