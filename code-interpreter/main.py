5d435edfd8529a85d3699ef832d9656b22e3e580
AdityaC4 <adityapchaudhari@gmail.com>
2025-04-20 19:19:43 -0400

    agent to run python code

--- code-interpreter/main.py
+++ code-interpreter/main.py
@@ -1,11 +1,46 @@
 from dotenv import load_dotenv
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
 
 load_dotenv()
 
 
 def main():
-    print("Hello World")
+    print("Starting...")
 
+    instructions = """
+    You are an agent designed to write code and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question.
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just say "I don't know" as the answer.
+    """
+    
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+    
+    tools = [PythonREPLTool()]
+    agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4o"),
+        tools=tools,
+    )
+    
+    agent_executor = AgentExecutor.from_agent_and_tools(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+    )
+    
+    agent_executor.invoke(
+        input={"input": """
+        generate and save in current working directory 15 QR codes that point to https://adityac4.github.io/
+        You have qrcode library installed.
+        """}
+    )
 
 if __name__ == "__main__":
     main()
